9dd8be83f38fbd9bdc739db49173cd07c712d96d
llc4rla <160608299+llc4rla@users.noreply.github.com>
2024-10-17 10:00:38 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é globalização?',
+    'Globalização é um processo de integração política, econômica e cultural mundial, marcado pelos avanços nos meios de transporte e comunicação'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Bom dia em Inglês?',
+    'Bom dia em ingles é Good Morning'
+)
